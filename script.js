e5c340c34274890be170ef3772278c883fd03d1a
RonnocX <135097884+RonnocX@users.noreply.github.com>
2023-06-24 22:05:19 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,86 @@
-const redAddButton = document.querySelector('#red-add');
-const redWarningButton = document.querySelector('#red-warning');
-const redMinusButton = document.querySelector('#red-minus');
-const redCount = document.querySelector('#red-count');
-let redScore = 0;
-let redWarnings = 0;
-
-const blueAddButton = document.querySelector('#blue-add');
-const blueWarningButton = document.querySelector('#blue-warning');
-const blueMinusButton = document.querySelector('#blue-minus');
-const blueCount = document.querySelector('#blue-count');
 let blueScore = 0;
-let blueWarnings = 0;
+let redScore = 0;
+let warnings = {
+    blue: 0,
+    red: 0
+};
 
-const resetButton = document.querySelector('#reset');
+const pointKeeper = document.getElementById('pointKeeper');
+pointKeeper.addEventListener('touchstart', handleTouchStart, false);
+pointKeeper.addEventListener('touchmove', handleTouchMove, false);
 
-redAddButton.addEventListener('click', () => {
-  redScore++;
-  redCount.textContent = redScore;
-});
+let xDown = null;
+let yDown = null;
 
-redWarningButton.addEventListener('click', () => {
-  redWarnings++;
-  if (redWarnings === 3) {
-    redScore--;
-    redCount.textContent = redScore;
-    redWarnings = 0;
-  } else {
-    redWarningButton.classList.add('warning');
-  }
-});
+function handleTouchStart(event) {
+    const firstTouch = event.touches[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
 
-redMinusButton.addEventListener('click', () => {
-  redScore--;
-  redCount.textContent = redScore;
-});
+function handleTouchMove(event) {
+    if (!xDown || !yDown) {
+        return;
+    }
 
-blueAddButton.addEventListener('click', () => {
-  blueScore++;
-  blueCount.textContent = blueScore;
-});
+    let xUp = event.touches[0].clientX;
+    let yUp = event.touches[0].clientY;
 
-blueWarningButton.addEventListener('click', () => {
-  blueWarnings++;
-  if (blueWarnings === 3) {
-    blueScore--;
-    blueCount.textContent = blueScore;
-    blueWarnings = 0;
-  } else {
-    blueWarningButton.classList.add('warning');
-  }
-});
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
 
-blueMinusButton.addEventListener('click', () => {
-  blueScore--;
-  blueCount.textContent = blueScore;
-});
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            // Swiped left
+            addPoint('red');
+        } else {
+            // Swiped right
+            addPoint('blue');
+        }
+    }
 
-resetButton.addEventListener('click', () => {
-  redScore = 0;
-  redCount.textContent = redScore;
-  redWarnings = 0;
-  redWarningButton.classList.remove('warning');
-  blueScore = 0;
-  blueCount.textContent = blueScore;
-  blueWarnings = 0;
-  blueWarningButton.classList.remove('warning');
-});
+    // Reset touch coordinates
+    xDown = null;
+    yDown = null;
+}
+
+function addPoint(color) {
+    if (color === 'blue') {
+        blueScore++;
+        document.getElementById('blueScore').getElementsByTagName('p')[0].textContent = blueScore;
+    } else if (color === 'red') {
+        redScore++;
+        document.getElementById('redScore').getElementsByTagName('p')[0].textContent = redScore;
+    }
+}
+
+function addWarning(color) {
+    warnings[color]++;
+    if (warnings[color] === 3) {
+        deductPoint(color);
+        warnings[color] = 0;
+    }
+}
+
+function deductPoint(color) {
+    if (color === 'blue') {
+        if (blueScore > 0) {
+            blueScore--;
+            document.getElementById('blueScore').getElementsByTagName('p')[0].textContent = blueScore;
+        }
+    } else if (color === 'red') {
+        if (redScore > 0) {
+            redScore--;
+            document.getElementById('redScore').getElementsByTagName('p')[0].textContent = redScore;
+        }
+    }
+}
+
+function resetScores() {
+    blueScore = 0;
+    redScore = 0;
+    warnings.blue = 0;
+    warnings.red = 0;
+    document.getElementById('blueScore').getElementsByTagName('p')[0].textContent = blueScore;
+    document.getElementById('redScore').getElementsByTagName('p')[0].textContent = redScore;
+}
